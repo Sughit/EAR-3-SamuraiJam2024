d88ccf83f6391247958d9bb7232036cc1a1f0d35
emanuel5867 <130173546+emanuel5867@users.noreply.github.com>
2024-01-26 01:41:08 +0200

    facut meniu ca luema si pus chestii faine

---
+++ EAR-3-SamuraiJam2024-game/Assets/Scripts/UI si meniu/MenuInGame.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuInGame : MonoBehaviour
+{
+    public GameObject menu;
+    public bool menuOpen = false;
+    public GameObject resumeButton, mainButton, quitButton, sagetuta;
+    public Text resumeText;
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape) && !menuOpen)
+        {
+            StartCoroutine(Butoane());
+            menuOpen = true;
+            menu.SetActive(true);
+        }
+        else if(Input.GetKeyDown(KeyCode.Escape) && menuOpen)
+        {
+            Time.timeScale = 1f;
+            menuOpen = false;
+
+            resumeButton.transform.localScale = new Vector2(1f, 1f);
+            sagetuta.SetActive(false);
+            resumeText.color = new Color(0.1960784f, 0.1960784f, 0.1960784f, 1);
+
+            quitButton.SetActive(false);
+            mainButton.SetActive(false);
+            resumeButton.SetActive(false);
+
+            menu.SetActive(false);
+        }
+        
+    }
+    public void MainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void Resume()
+    {
+        Time.timeScale = 1f;
+        menuOpen = false;
+
+        resumeButton.transform.localScale = new Vector2(1f, 1f);
+        sagetuta.SetActive(false);
+        resumeText.color = new Color(0.1960784f, 0.1960784f, 0.1960784f, 1);
+
+        quitButton.SetActive(false);
+        mainButton.SetActive(false);
+        resumeButton.SetActive(false);
+
+        menu.SetActive(false);
+        
+    }
+
+    IEnumerator Butoane()
+    {
+        quitButton.SetActive(true);
+        yield return new WaitForSeconds(0.08f);
+        mainButton.SetActive(true);
+        yield return new WaitForSeconds(0.08f);
+        resumeButton.SetActive(true);
+        Time.timeScale = 0f;
+    }
+}
