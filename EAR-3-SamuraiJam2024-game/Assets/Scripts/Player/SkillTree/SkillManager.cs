0caaa247af37e06388ad0e6efc10933a0afd9a90
Irimia-Sorici Andrei <andreisorici2007@gmail.com>
2024-01-28 09:55:26 +0200

    poti sa iei heal

--- EAR-3-SamuraiJam2024-game/Assets/Scripts/Player/SkillTree/SkillManager.cs
+++ EAR-3-SamuraiJam2024-game/Assets/Scripts/Player/SkillTree/SkillManager.cs
@@ -4,6 +4,26 @@ using UnityEngine;
 
 public class SkillManager : MonoBehaviour
 {
+    public void Heal()
+    {
+        if(GetComponent<UnlockSkills>().cost <= UnlockSkills.numSkillPoints)
+        {
+            if(GameObject.FindWithTag("Player").GetComponent<PlayerHealth>().health < GameObject.FindWithTag("Player").GetComponent<PlayerHealth>().maxHealth)
+            {
+                if(GameObject.FindWithTag("Player").GetComponent<PlayerHealth>().health <= GameObject.FindWithTag("Player").GetComponent<PlayerHealth>().maxHealth - 10f)
+                {
+                    GameObject.FindWithTag("Player").GetComponent<PlayerHealth>().health += 10f;
+                    UnlockSkills.numSkillPoints -= GetComponent<UnlockSkills>().cost;
+                }
+                else 
+                {
+                    GameObject.FindWithTag("Player").GetComponent<PlayerHealth>().health = GameObject.FindWithTag("Player").GetComponent<PlayerHealth>().maxHealth;
+                    UnlockSkills.numSkillPoints -= GetComponent<UnlockSkills>().cost;
+                }
+            }
+        }
+    }
+
     public void Shuriken1()
     {
         if(GetComponent<UnlockSkills>().cost <= UnlockSkills.numSkillPoints)
