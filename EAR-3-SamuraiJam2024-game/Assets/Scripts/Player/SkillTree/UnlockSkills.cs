370766c3d2b79e6a592a6e0a4b4962b2b4a41ded
Irimia-Sorici Andrei <andreisorici2007@gmail.com>
2024-01-28 09:40:28 +0200

    merg shurikenurile

--- EAR-3-SamuraiJam2024-game/Assets/Scripts/Player/SkillTree/UnlockSkills.cs
+++ EAR-3-SamuraiJam2024-game/Assets/Scripts/Player/SkillTree/UnlockSkills.cs
@@ -13,7 +13,7 @@ public class UnlockSkills : MonoBehaviour
     public Text costText;
     public Text skillPointsText;
 
-    public static int numSkillPoints=0;
+    public static int numSkillPoints=10;
 
     void Start()
     {
